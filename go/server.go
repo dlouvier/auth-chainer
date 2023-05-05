faab7d66fd1a958304a86535d262790e91843ec2
Daniel Louvier <13359249+dlouvier@users.noreply.github.com>
2023-05-05 19:12:15 +0200

    working state

--- go/server.go
+++ go/server.go
@@ -4,100 +4,165 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
+	"os"
 
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 )
 
+type UserSession struct {
+	Valid  bool
+	UserId string
+}
+
+func NewUserSession() *UserSession {
+	return &UserSession{
+		Valid:  false,
+		UserId: "",
+	}
+}
+
 type ExternalAuthServices struct {
-	AuthentificationClient     *http.Client
-	AuthentificationServiceUrl string
-	AuthorisationClient        *http.Client
-	AuthorisationServiceUrl    string
-	UserAuthentificated        bool
-	UserAuthorised             bool
-	UserId                     string
+	AuthentificationClient      *http.Client
+	AuthentificationServiceHost string
+	AuthorisationClient         *http.Client
+	AuthorisationServiceHost    string
 }
 
 func NewExternalAuthServices() *ExternalAuthServices {
+	authentificationServiceHost, envVarSet := os.LookupEnv("AUTHENTIFICATION_SERVICE_HOST")
+	if !envVarSet {
+		log.Fatalln("AUTHENTIFICATION_SERVICE_HOST enviroment variable is not set.")
+	}
+	authorisationServiceHost, envVarSet := os.LookupEnv("AUTHORISATION_SERVICE_HOST")
+	if !envVarSet {
+		log.Fatalln("AUTHORISATION_SERVICE_HOST enviroment variable is not set.")
+	}
+
 	return &ExternalAuthServices{
-		AuthentificationClient:     http.DefaultClient,
-		AuthentificationServiceUrl: "https://www.google.es",
-		AuthorisationClient:        http.DefaultClient,
-		AuthorisationServiceUrl:    "https://www.google.es",
-		UserAuthentificated:        false,
-		UserAuthorised:             false,
+		AuthentificationClient:      http.DefaultClient,
+		AuthentificationServiceHost: authentificationServiceHost,
+		AuthorisationClient:         http.DefaultClient,
+		AuthorisationServiceHost:    authorisationServiceHost,
 	}
 }
 
-func (eas *ExternalAuthServices) CheckAuthentification(request *http.Request) {
-	url, _ := url.Parse(eas.AuthentificationServiceUrl)
-	request.Host = request.Header.Get("X-Forwarded-Host")
+func (eas *ExternalAuthServices) isUserAuthentificated(request *http.Request) (bool, string, error) {
+	request.Host = GetHostFromRequest(request)
 	request.RequestURI = ""
-	request.URL.Host = url.Host
-	request.URL.Path = url.Path
-	request.URL.Scheme = url.Scheme
+	request.URL.Host = eas.AuthentificationServiceHost
+	request.URL.Path = "/oauth2/auth"
+	request.URL.Scheme = "http"
+	response, err := eas.AuthentificationClient.Do(request)
+
+	if err != nil {
+		return false, "", err
+	}
+	if response.StatusCode == 202 {
+		userId := response.Header.Get("X-Auth-Request-User")
+		return true, userId, nil
+	}
+	return false, "", nil
+}
+
+func (eas *ExternalAuthServices) isUserAuthorised(request *http.Request) (bool, error) {
+	request.Host = GetHostFromRequest(request)
+	request.RequestURI = ""
+	request.URL.Host = eas.AuthorisationServiceHost
+	request.URL.Path = "/webauthn/auth"
+	request.URL.Scheme = "http"
 	response, err := eas.AuthentificationClient.Do(request)
 	if err != nil {
-		log.Printf("client: error making http request: %s\n", err)
+		return false, err
 	}
-	if (response.StatusCode >= 200) && (response.StatusCode <= 202) {
-		eas.UserId = response.Header.Get("X-Auth-Request-User")
-		eas.UserAuthentificated = true
+	log.Println(response.StatusCode)
+	if response.StatusCode == 200 {
+		return true, nil
 	}
+	return false, nil
 }
 
-func (eas *ExternalAuthServices) CheckAuthorisation(request *http.Request) {
-	url, _ := url.Parse(eas.AuthorisationServiceUrl)
-	request.Host = request.Header.Get("X-Forwarded-Host")
-	request.RequestURI = ""
-	request.URL.Host = url.Host
-	request.URL.Path = url.Path
-	request.URL.Scheme = url.Scheme
-	response, err := eas.AuthentificationClient.Do(request)
+func AuthHandler(c echo.Context) error {
+	sess, err := session.Get("session", c)
 	if err != nil {
-		log.Printf("client: error making http request: %s\n", err)
+		log.Println("Error obtaining the session")
+		log.Println(err)
 	}
-	if (response.StatusCode >= 200) && (response.StatusCode <= 202) {
-		eas.UserAuthorised = true
+
+	userSession, ok := sess.Values["session"].(UserSession)
+	if !ok {
+		log.Println("User session is missing,")
+		return c.String(http.StatusUnauthorized, "Unable to auth")
 	}
-}
 
-func (eas *ExternalAuthServices) AuthHandler(c echo.Context) error {
-	eas.CheckAuthentification(c.Request())
-	eas.CheckAuthorisation(c.Request())
-
-	if eas.UserAuthentificated && eas.UserAuthorised {
-		return c.String(http.StatusAccepted, fmt.Sprintf("Hello, %s", eas.UserId))
+	if userSession.Valid {
+		c.Response().Header().Set("X-Auth-Request-User", userSession.UserId)
+		return c.String(http.StatusAccepted, fmt.Sprintf("Hello, %s", userSession.UserId))
 	} else {
 		return c.String(http.StatusUnauthorized, "Unable to auth")
 	}
 }
 
 func (eas *ExternalAuthServices) RegisterHandler(c echo.Context) error {
-	eas.CheckAuthentification(c.Request())
-	eas.CheckAuthorisation(c.Request())
+	host := GetHostFromRequest(c.Request())
 
-	log.Println("DANIIIIIIIIIIIIIIIII")
-	log.Println(c.Request())
-
-	if !eas.UserAuthentificated {
-		return c.Redirect(http.StatusTemporaryRedirect, fmt.Sprintf("http://%s/oauth2/sign_in", c.Request().Host))
+	authentificated, userId, err := eas.isUserAuthentificated(c.Request())
+	if err != nil {
+		log.Printf("There was an error trying to authentificate the user: \n%s", err)
+		return err
+	}
+	if !authentificated {
+		return c.Redirect(http.StatusTemporaryRedirect, fmt.Sprintf("%s://%s/oauth2/sign_in", c.Scheme(), host))
 	}
 
-	if !eas.UserAuthorised {
+	authorised, err := eas.isUserAuthorised(c.Request())
+	if err != nil {
+		log.Printf("There was an error trying to authorise the device: \n%s", err)
+		return err
+	}
+	if !authorised {
 		return c.Redirect(http.StatusTemporaryRedirect, fmt.Sprintf(
-			"http://%s/webauthn/login?redirect_url=http://%s&default_username=%s",
-			c.Request().Host,
-			c.Request().Host,
-			eas.UserId))
+			"%s://%s/webauthn/login?redirect_url=%s://%s&default_username=%s",
+			c.Scheme(),
+			host,
+			c.Scheme(),
+			host,
+			userId))
 	}
 
-	if eas.UserAuthentificated && eas.UserAuthorised {
-		return c.String(http.StatusAccepted, "ALL DONE HERE")
+	if authentificated && authorised {
+		sess, err := session.Get("session", c)
+		if err != nil {
+			log.Printf("There was an error trying obtain the user session: \n%s", err)
+			return c.NoContent(http.StatusInternalServerError)
+		}
+
+		sess.Values["session"] = UserSession{
+			Valid:  true,
+			UserId: userId,
+		}
+
+		sess.Save(c.Request(), c.Response())
+
+		c.Response().Header().Set("X-Auth-Request-User", userId)
+
+		return c.String(http.StatusAccepted, fmt.Sprintf("Hello, %s", userId))
 	} else {
-		return c.String(http.StatusUnauthorized, "Unable to auth")
+		return c.NoContent(http.StatusInternalServerError)
 	}
+}
+
+func GetHostFromRequest(request *http.Request) string {
+	var host string
+
+	if len(request.Header.Get("X-Forwarded-Host")) > 0 {
+		host = request.Header.Get("X-Forwarded-Host")
+	} else {
+		host = request.Host
+	}
+
+	return host
 }
 
 func main() {
@@ -108,7 +173,9 @@ func main() {
 	// Debug mode
 	e.Debug = true
 
-	e.GET("/auth", eas.AuthHandler)
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
+
+	e.GET("/auth", AuthHandler)
 
 	e.GET("/register", eas.RegisterHandler)
 
--- go/server_test.go
+++ go/server_test.go
@@ -2,135 +2,143 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
 func mockupServer(port int, responseCode int, responseHeaders map[string]string) *httptest.Server {
 	mock := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		for key, value := range responseHeaders {
-			w.Header().Add(key, value)
+		if strings.HasPrefix(r.URL.Path, "/oauth2") || strings.HasPrefix(r.URL.Path, "/webauthn") {
+			for key, value := range responseHeaders {
+				w.Header().Add(key, value)
+			}
+			w.WriteHeader(responseCode)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
 		}
-		w.WriteHeader(responseCode)
 	}))
 	AuthenticationServer, _ := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	mock.Listener = AuthenticationServer
 	mock.Start()
 	return mock
 }
-func TestAuthHandlerUserIsAuthentificatedAndAuthorised(t *testing.T) {
-	AuthentificationServer := mockupServer(9090, http.StatusUnauthorized, nil)
-	defer AuthentificationServer.Close()
 
-	AuthorisationServer := mockupServer(9091, http.StatusUnauthorized, nil)
-	defer AuthorisationServer.Close()
+func TestConstructorNewExternalAuthServices(t *testing.T) {
+	t.Setenv("AUTHENTIFICATION_SERVICE_HOST", "oauth2.default.svc.cluster.local")
+	t.Setenv("AUTHORISATION_SERVICE_HOST", "webauthn.default.svc.cluster.local")
 
-	eas := ExternalAuthServices{
-		AuthentificationClient:     AuthentificationServer.Client(),
-		AuthentificationServiceUrl: "http://localhost:9090",
-		AuthorisationClient:        AuthorisationServer.Client(),
-		AuthorisationServiceUrl:    "http://localhost:9091",
-		UserAuthentificated:        true,
-		UserAuthorised:             true,
-		UserId:                     "T3stUs3r",
-	}
+	eas := NewExternalAuthServices()
 
+	assert.Equal(t, eas.AuthentificationServiceHost, "oauth2.default.svc.cluster.local")
+	assert.Equal(t, eas.AuthorisationServiceHost, "webauthn.default.svc.cluster.local")
+}
+
+func TestAuthHandlerUserIsValid(t *testing.T) {
 	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/auth", nil)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 
-	if assert.NoError(t, eas.AuthHandler(c)) {
+	// https://github.com/labstack/echo/issues/1447
+	c.Set("_session_store", sessions.NewCookieStore([]byte("secret")))
+
+	sess, err := session.Get("session", c)
+	if err != nil {
+		log.Println("Error obtaining the session")
+		log.Println(err)
+
+	}
+
+	sess.Values["session"] = UserSession{
+		Valid:  true,
+		UserId: "t3stus3r",
+	}
+
+	sess.Save(c.Request(), c.Response())
+
+	if assert.NoError(t, AuthHandler(c)) {
 		assert.Equal(t, http.StatusAccepted, rec.Code)
-		assert.Equal(t, fmt.Sprintf("Hello, %s", eas.UserId), string(rec.Body.String()))
+		assert.Equal(t, "Hello, t3stus3r", string(rec.Body.String()))
 	}
 }
-func TestAuthHandlerUserIsAuthentificatedButNotAuthorised(t *testing.T) {
-	AuthentificationServer := mockupServer(9090, http.StatusUnauthorized, nil)
-	defer AuthentificationServer.Close()
 
-	AuthorisationServer := mockupServer(9091, http.StatusUnauthorized, nil)
-	defer AuthorisationServer.Close()
-
-	eas := ExternalAuthServices{
-		AuthentificationClient:     AuthentificationServer.Client(),
-		AuthentificationServiceUrl: "http://localhost:9090",
-		AuthorisationClient:        AuthorisationServer.Client(),
-		AuthorisationServiceUrl:    "http://localhost:9091",
-		UserAuthentificated:        true,
-		UserAuthorised:             false,
-		UserId:                     "T3stUs3r",
-	}
-
-	// Setup
+func TestAuthHandlerUserIsNotValid(t *testing.T) {
 	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/auth", nil)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 
-	// Assertions
-	if assert.NoError(t, eas.AuthHandler(c)) {
+	// https://github.com/labstack/echo/issues/1447
+	c.Set("_session_store", sessions.NewCookieStore([]byte("secret")))
+
+	sess, err := session.Get("session", c)
+	if err != nil {
+		log.Println("Error obtaining the session")
+		log.Println(err)
+	}
+
+	sess.Values["session"] = UserSession{
+		Valid:  false,
+		UserId: "",
+	}
+
+	sess.Save(c.Request(), c.Response())
+
+	if assert.NoError(t, AuthHandler(c)) {
 		assert.Equal(t, http.StatusUnauthorized, rec.Code)
 	}
 }
-func TestAuthHandlerUserNotAuthentificatedButIsAuthorised(t *testing.T) {
-	AuthentificationServer := mockupServer(9090, http.StatusUnauthorized, nil)
-	defer AuthentificationServer.Close()
 
-	AuthorisationServer := mockupServer(9091, http.StatusUnauthorized, nil)
-	defer AuthorisationServer.Close()
-
-	eas := ExternalAuthServices{
-		AuthentificationClient:     AuthentificationServer.Client(),
-		AuthentificationServiceUrl: "http://localhost:9090",
-		AuthorisationClient:        AuthorisationServer.Client(),
-		AuthorisationServiceUrl:    "http://localhost:9091",
-		UserAuthentificated:        false,
-		UserAuthorised:             true,
-	}
-
-	// Setup
+func TestAuthHandlerSessionDoesNotExist(t *testing.T) {
 	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/auth", nil)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 
-	// Assertions
-	if assert.NoError(t, eas.AuthHandler(c)) {
+	// https://github.com/labstack/echo/issues/1447
+	c.Set("_session_store", sessions.NewCookieStore([]byte("secret")))
+
+	if assert.NoError(t, AuthHandler(c)) {
 		assert.Equal(t, http.StatusUnauthorized, rec.Code)
 	}
 }
 
 func TestRegisterHandlerUserIsAuthentificatedAndAuthorised(t *testing.T) {
-	AuthentificationServer := mockupServer(9090, http.StatusUnauthorized, nil)
+	AuthentificationServer := mockupServer(9090, http.StatusAccepted, map[string]string{
+		"X-Auth-Request-User": "T3stUs3r",
+	})
 	defer AuthentificationServer.Close()
 
-	AuthorisationServer := mockupServer(9091, http.StatusUnauthorized, nil)
+	AuthorisationServer := mockupServer(9091, http.StatusOK, nil)
 	defer AuthorisationServer.Close()
 
 	eas := ExternalAuthServices{
-		AuthentificationClient:     AuthentificationServer.Client(),
-		AuthentificationServiceUrl: "http://localhost:9090",
-		AuthorisationClient:        AuthorisationServer.Client(),
-		AuthorisationServiceUrl:    "http://localhost:9091",
-		UserAuthentificated:        true,
-		UserAuthorised:             true,
-		UserId:                     "T3stUs3r",
+		AuthentificationClient:      AuthentificationServer.Client(),
+		AuthentificationServiceHost: "127.0.0.1:9090",
+		AuthorisationClient:         AuthorisationServer.Client(),
+		AuthorisationServiceHost:    "127.0.0.1:9091",
 	}
 
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/auth", nil)
+	req := httptest.NewRequest(http.MethodGet, "/register", nil)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
+	// https://github.com/labstack/echo/issues/1447
+	c.Set("_session_store", sessions.NewCookieStore([]byte("secret")))
 
-	if assert.NoError(t, eas.AuthHandler(c)) {
+	if assert.NoError(t, eas.RegisterHandler(c)) {
 		assert.Equal(t, http.StatusAccepted, rec.Code)
-		assert.Equal(t, fmt.Sprintf("Hello, %s", eas.UserId), string(rec.Body.String()))
+		assert.Equal(t, "Hello, T3stUs3r", string(rec.Body.String()))
+		assert.Equal(t, "T3stUs3r", rec.Header().Get("X-Auth-Request-User"))
 	}
 }
 
@@ -142,14 +150,14 @@ func TestRegisterHandlerUserIsNotAuthentificated(t *testing.T) {
 	defer AuthorisationServer.Close()
 
 	eas := ExternalAuthServices{
-		AuthentificationClient:     AuthentificationServer.Client(),
-		AuthentificationServiceUrl: "http://localhost:9090",
-		AuthorisationClient:        AuthorisationServer.Client(),
-		AuthorisationServiceUrl:    "http://localhost:9091",
+		AuthentificationClient:      AuthentificationServer.Client(),
+		AuthentificationServiceHost: "127.0.0.1:9090",
+		AuthorisationClient:         AuthorisationServer.Client(),
+		AuthorisationServiceHost:    "127.0.0.1:9091",
 	}
 
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "https://localhost/register", nil)
+	req := httptest.NewRequest(http.MethodGet, "http://localhost/register", nil)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 
@@ -162,17 +170,19 @@ func TestRegisterHandlerUserIsNotAuthentificated(t *testing.T) {
 }
 
 func TestRegisterHandlerUserIsNotAuthorised(t *testing.T) {
-	AuthentificationServer := mockupServer(9090, http.StatusAccepted, nil)
+	AuthentificationServer := mockupServer(9090, http.StatusAccepted, map[string]string{
+		"X-Auth-Request-User": "T3stUs3r",
+	})
 	defer AuthentificationServer.Close()
 
 	AuthorisationServer := mockupServer(9091, http.StatusUnauthorized, nil)
 	defer AuthorisationServer.Close()
 
 	eas := ExternalAuthServices{
-		AuthentificationClient:     AuthentificationServer.Client(),
-		AuthentificationServiceUrl: "http://localhost:9090",
-		AuthorisationClient:        AuthorisationServer.Client(),
-		AuthorisationServiceUrl:    "http://localhost:9091",
+		AuthentificationClient:      AuthentificationServer.Client(),
+		AuthentificationServiceHost: "127.0.0.1:9090",
+		AuthorisationClient:         AuthorisationServer.Client(),
+		AuthorisationServiceHost:    "127.0.0.1:9091",
 	}
 
 	e := echo.New()
@@ -182,7 +192,7 @@ func TestRegisterHandlerUserIsNotAuthorised(t *testing.T) {
 
 	if assert.NoError(t, eas.RegisterHandler(c)) {
 		assert.Equal(t, http.StatusTemporaryRedirect, rec.Code)
-		expectedUrl := "http://localhost/oauth2/sign_in"
+		expectedUrl := "http://localhost/webauthn/login?redirect_url=http://localhost&default_username=T3stUs3r"
 		actualUrl, _ := rec.Result().Location()
 		assert.Equal(t, expectedUrl, actualUrl.String())
 	}
